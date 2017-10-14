5ebd30ba47fa20bd61a36199d1f15f2ef9b8b620
Nikolay Kipnyak <nikolaykipnyak@Nikolays-MacBook-Pro.local>
2017-10-14 09:07:40 +0300

    add logout button to main scene

--- src/Router.js
+++ src/Router.js
@@ -6,18 +6,20 @@ import ItemCreate from './components/ItemCreate';
 
 const RouterComponent = () => {
   return (
-    <Router sceneStyle={{ paddingTop: 65, backgroundColor:'#6495ED' }}>
+    <Router sceneStyle={{ paddingTop: 65,backgroundColor:'#6495ED'}}>
         <Scene key="auth" hideNavBar={true}>
           <Scene key="login" component={LoginForm} title="Please Login" initial/>
         </Scene>
 
-        <Scene key="main">
+        <Scene key="main" sceneStyle={{backgroundColor:'#fff'}}>
           <Scene
             key="ItemList"
             component={ItemList}
             title="Item List"
-            rightTitle="Add"
-            onRight={() => Actions.ItemCreate()}
+            rightTitle="Log Out"
+            leftTitle="Add"
+            onLeft={() => Actions.ItemCreate()}
+            onRight={() => Actions.auth()}
             initial
           />
           <Scene key="ItemCreate" component={ItemCreate} title="New Item" />
